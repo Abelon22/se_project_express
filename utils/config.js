de40bd12093e560fab3db2d988efb642b9f991bb
Abelon22 <overtonbellington@gmail.com>
2025-10-17 13:00:36 -0400

    changed env in config

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,7 @@
-const JWT_SECRET = "overton-secret-code";
+require("dotenv").config();
+
 const MONGO_URI = "mongodb://127.0.0.1:27017/wtwr_db";
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3001, JWT_SECRET = "overton-secret-code" } = process.env;
 
 module.exports = { JWT_SECRET, MONGO_URI, PORT };
