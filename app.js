f051cb60f03a59d4b9f22efbb598006c4efff1c6
Abelon22 <overtonbellington@gmail.com>
2025-10-17 18:13:40 -0400

    cors

--- app.js
+++ app.js
@@ -24,8 +24,18 @@ connectToMongoose()
   });
 
 const app = express();
+const allowedOrigins = [
+  "https://wtwr.lookids.com",
+  "https://www.wtwr.lookids.com",
+  "https://api.wtwr.lookids.com", // usually not needed as an Origin, but ok to include
+];
 
-app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true, // if you send cookies/authorization
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
